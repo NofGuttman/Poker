947fd0c3a722c9ae8ce2ccd08e4a8255a571f068
Nof Guttman <nofguttman@gmail.com>
2017-08-04 16:41:47 +0300

    new winner selector file

---
+++ app/winner.js
@@ -0,0 +1,35 @@
+function winner(players) {
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//player1 = {
+//    userName: "example1",
+//    rank: [0, 14, 5, 4, 3, 2],
+//}
+
+let example = [
+    {
+        userName: "player1",
+        rank: [0, 13, 10, 9, 8, 4]
+    }
+]
+
+console.log()
