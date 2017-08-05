5cfc565ecb224e8b3369a047c51924296959cd32
Nof Guttman <nofguttman@gmail.com>
2017-08-05 19:37:20 +0300

    winner.js is working + test suite passed!

--- app/winner.js
+++ app/winner.js
@@ -5,7 +5,6 @@ function winner(players){
         playersRank.push([players[i]["userName"], players[i]["rank"]]);
         toCheck.push(players[i]["userName"]);
     }
-    console.log(toCheck);
     for(let place = 0; place < 7; place++){
         for(let rate = 14; rate > 0; rate--){
             let winners = [];
@@ -19,12 +18,12 @@ function winner(players){
             }
             if(winners.length == 1){
                 return winners;
-            } else if(winners > 1){
+            } else if(winners.length > 1){
                 [toCheck, winners] = [winners, []]
             }
         }
     }
-    return winners;
+    return toCheck;
 }
 
 
@@ -40,8 +39,8 @@ function winner(players){
 
 let example = [
     {"userName": "player1", "rank": [4, 10]},
-    {"userName": "player2", "rank": [1, 13, 9, 8, 4]},
-    {"userName": "player3", "rank": [4, 11]}
+    {"userName": "player2", "rank": [2, 13, 9, 8, 4]},
+    {"userName": "player3", "rank": [4, 10]}
 ]
 
 
--- tests/winner.test.js
+++ tests/winner.test.js
@@ -1,33 +1,33 @@
 const winner = require('../app/winner');
 
 test("Winner", () =>{
-    expect([
+    expect(winner([
         {userName: "Player1", rank: [0, 14, 13, 10, 8, 7]},
         {userName: "Player2", rank: [1, 11, 14, 6, 5]},
         {userName: "Player3", rank: [0, 12, 10, 9, 8, 7]}
-    ]).toBe(["Player2"]);
+    ])).toEqual(["Player2"]);
 });
 
 test("Winner", () =>{
-    expect([
+    expect(winner([
         {userName: "Player1", rank: [2, 13, 11, 8]},
         {userName: "Player2", rank: [1, 11, 14, 6, 5]},
         {userName: "Player3", rank: [0, 12, 10, 9, 8, 7]}
-    ]).toBe(["Player1"]);
+    ])).toEqual(["Player1"]);
 });
 
 test("Winner", () =>{
-    expect([
+    expect(winner([
         {userName: "Player1", rank: [4, 10]},
         {userName: "Player2", rank: [1, 11, 14, 6, 5]},
         {userName: "Player3", rank: [4, 13]}
-    ]).toBe(["Player3"]);
+    ])).toEqual(["Player3"]);
 });
 
 test("Winner", () =>{
-    expect([
+    expect(winner([
         {userName: "Player1", rank: [1, 14, 13, 10, 8, 7]},
         {userName: "Player2", rank: [1, 14, 13, 10, 8, 7]},
         {userName: "Player3", rank: [0, 12, 10, 9, 8, 7]}
-    ]).toBe(["Player1", "Player2"]);
+    ])).toEqual(["Player1", "Player2"]);
 });
