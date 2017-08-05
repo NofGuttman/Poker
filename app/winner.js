ff6123834bf87b703a7569eb8269fff6a2d65eef
Nof Guttman <nofguttman@gmail.com>
2017-08-05 19:25:58 +0300

    before changing winner.js (not working yet)

--- app/winner.js
+++ app/winner.js
@@ -1,5 +1,30 @@
 function winner(players){
-    return players;
+    let playersRank = [];
+    let toCheck = [];
+    for(let i in players){
+        playersRank.push([players[i]["userName"], players[i]["rank"]]);
+        toCheck.push(players[i]["userName"]);
+    }
+    console.log(toCheck);
+    for(let place = 0; place < 7; place++){
+        for(let rate = 14; rate > 0; rate--){
+            let winners = [];
+            for(let player in playersRank){
+                if(toCheck.includes(playersRank[player][0])){
+                    let rank = playersRank[player][1];
+                    if(rank[place] == rate){
+                        winners.push(playersRank[player][0])
+                    }
+                }
+            }
+            if(winners.length == 1){
+                return winners;
+            } else if(winners > 1){
+                [toCheck, winners] = [winners, []]
+            }
+        }
+    }
+    return winners;
 }
 
 
@@ -14,10 +39,12 @@ function winner(players){
 
 
 let example = [
-    {userName: "player1", rank: [0, 13, 10, 9, 8, 4]},
-    {userName: "player2", rank: [0, 13, 10, 9, 8, 4]},
-    {userName: "player3", rank: [0, 13, 10, 9, 8, 4]},
-    {userName: "player4", rank: [0, 13, 10, 9, 8, 4]}
+    {"userName": "player1", "rank": [4, 10]},
+    {"userName": "player2", "rank": [1, 13, 9, 8, 4]},
+    {"userName": "player3", "rank": [4, 11]}
 ]
+
+
+console.log(winner(example)); // output ==> ["player3"]
 
 module.exports = winner;
--- tests/winner.test.js
+++ tests/winner.test.js
@@ -7,3 +7,27 @@ test("Winner", () =>{
         {userName: "Player3", rank: [0, 12, 10, 9, 8, 7]}
     ]).toBe(["Player2"]);
 });
+
+test("Winner", () =>{
+    expect([
+        {userName: "Player1", rank: [2, 13, 11, 8]},
+        {userName: "Player2", rank: [1, 11, 14, 6, 5]},
+        {userName: "Player3", rank: [0, 12, 10, 9, 8, 7]}
+    ]).toBe(["Player1"]);
+});
+
+test("Winner", () =>{
+    expect([
+        {userName: "Player1", rank: [4, 10]},
+        {userName: "Player2", rank: [1, 11, 14, 6, 5]},
+        {userName: "Player3", rank: [4, 13]}
+    ]).toBe(["Player3"]);
+});
+
+test("Winner", () =>{
+    expect([
+        {userName: "Player1", rank: [1, 14, 13, 10, 8, 7]},
+        {userName: "Player2", rank: [1, 14, 13, 10, 8, 7]},
+        {userName: "Player3", rank: [0, 12, 10, 9, 8, 7]}
+    ]).toBe(["Player1", "Player2"]);
+});
