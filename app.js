9b4789f8c7a216ca90c40fea40f0848e04fa9583
Nof Guttman <nofguttman@gmail.com>
2017-08-06 16:59:13 +0300

    Trying new index2.html file for tests and playing

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.static('view'));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/view/index.html');
+    res.sendFile(__dirname + '/view/index2.html');
 });
 
 app.get('/getDeck', function(req, res){
