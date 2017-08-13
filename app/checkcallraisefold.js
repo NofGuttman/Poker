4d880b5c56d4465dc4cfa8a6bf2b331749e0fd9b
Nof Guttman <nofguttman@gmail.com>
2017-08-13 23:26:00 +0300

    new file for bidding

---
+++ app/checkcallraisefold.js
@@ -0,0 +1,28 @@
+let playersInThisRound = [
+    {userName: "Player1", goodToGo: false, money: 1000},
+    {userName: "Player2", goodToGo: false, money: 1000},
+    {userName: "Player3", goodToGo: false, money: 1000}
+];
+
+function bid(playerName, action, players, amount){
+    switch(action){
+        case "CHECK":
+            
+            break;
+        case "FOLD":
+            
+            break;
+        case "RAISE":
+            
+            break;
+        case "CALL":
+            
+            break;
+    }
+}
+
+
+//fold
+//check
+//call
+//raise
