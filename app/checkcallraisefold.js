ab2af77b9c972de13ac676612e4455631cf65a59
Nof Guttman <nofguttman@gmail.com>
2017-08-15 15:55:36 +0300

    Progress in bid system

--- app/checkcallraisefold.js
+++ app/checkcallraisefold.js
@@ -1,10 +1,17 @@
-let playersInThisRound = [
-    {userName: "Player1", goodToGo: false, money: 10000, call: 0, selfBid: 0},
-    {userName: "Player2", goodToGo: false, money: 20000, call: 0, selfBid: 0},
-    {userName: "Player3", goodToGo: false, money: 30000, call: 0, selfBid: 0}
-];
+const player1 = {userName: "Player1", goodToGo: false, fold: false, money: 10000, call: 0, selfBid: 0}
+const player2 = {userName: "Player2", goodToGo: false, fold: false, money: 20000, call: 0, selfBid: 0}
+const player3 = {userName: "Player3", goodToGo: false, fold: false, money: 30000, call: 0, selfBid: 0}
 
-const player1 = {userName: "Player1"}
+let playersInThisRound = [player1, player2, player3];
+
+function everyBodyChecked(players){
+    for(let i in players){
+        if(!(players[i].goodToGo || players[i].fold)){
+            return false;
+        }
+    }
+    return true;
+}
 
 function bid(action, player, players, bidValue){
     let relevantPlayer
@@ -38,10 +45,14 @@ function bid(action, player, players, bidValue){
             break;
         
         case "FOLD":
+            relevantPlayer.fold = true;
             break;
     }
+    console.log(everyBodyChecked(players));
 }
 
+bid("CHECK", player3, playersInThisRound, 0);
 bid("RAISE", player1, playersInThisRound, 100);
-            
+bid("FOLD", player2, playersInThisRound, 0);
+bid("CALL", player3, playersInThisRound, 0);
 console.log(playersInThisRound);
