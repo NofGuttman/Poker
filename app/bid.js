bf528f4076b6ed7352244f529433114c3236b053
Nof Guttman <nofguttman@gmail.com>
2017-08-18 19:21:45 +0300

    bid file

--- app/bid.js
+++ app/bid.js
@@ -1,8 +1,8 @@
-const player1 = {userName: "Player1", goodToGo: false, fold: false, money: 10000, call: 0, selfBid: 0}
-const player2 = {userName: "Player2", goodToGo: false, fold: false, money: 20000, call: 0, selfBid: 0}
-const player3 = {userName: "Player3", goodToGo: false, fold: false, money: 30000, call: 0, selfBid: 0}
-
-let playersInThisRound = [player1, player2, player3];
+//const player1 = {userName: "Player1", goodToGo: false, fold: false, money: 10000, call: 0, selfBid: 0}
+//const player2 = {userName: "Player2", goodToGo: false, fold: false, money: 20000, call: 0, selfBid: 0}
+//const player3 = {userName: "Player3", goodToGo: false, fold: false, money: 30000, call: 0, selfBid: 0}
+//
+//let playersInThisRound = [player1, player2, player3];
 
 function everyBodyChecked(players){
     for(let i in players){
@@ -43,16 +43,18 @@ function bid(action, player, players, bidValue){
             [relevantPlayer.selfBid, relevantPlayer.call] = [relevantPlayer.call + bidValue, 0];
             relevantPlayer.money -= relevantPlayer.selfBid;
             break;
-        
+            
         case "FOLD":
             relevantPlayer.fold = true;
             break;
     }
-    console.log(everyBodyChecked(players));
+    return everyBodyChecked(players);
 }
 
-bid("CHECK", player3, playersInThisRound, 0);
-bid("RAISE", player1, playersInThisRound, 100);
-bid("FOLD", player2, playersInThisRound, 0);
-bid("CALL", player3, playersInThisRound, 0);
-console.log(playersInThisRound);
+module.exports = bid;
+
+//console.log(bid("CHECK", player3, playersInThisRound, 0));
+//bid("RAISE", player1, playersInThisRound, 100);
+//bid("FOLD", player2, playersInThisRound, 0);
+//console.log(bid("CALL", player3, playersInThisRound, 0));
+//console.log(playersInThisRound);
