c7872aeaa978da3726e2bc80456ad3467d84a7b1
Nof Guttman <nofguttman@gmail.com>
2017-08-19 17:20:35 +0300

    added end of round stuff

--- app/bid.js
+++ app/bid.js
@@ -1,8 +1,10 @@
-//const player1 = {userName: "Player1", goodToGo: false, fold: false, money: 10000, call: 0, selfBid: 0}
-//const player2 = {userName: "Player2", goodToGo: false, fold: false, money: 20000, call: 0, selfBid: 0}
-//const player3 = {userName: "Player3", goodToGo: false, fold: false, money: 30000, call: 0, selfBid: 0}
-//
-//let playersInThisRound = [player1, player2, player3];
+const player1 = {userName: "Player1", goodToGo: false, fold: false, money: 10000, call: 0, selfBid: 0}
+const player2 = {userName: "Player2", goodToGo: false, fold: false, money: 20000, call: 0, selfBid: 0}
+const player3 = {userName: "Player3", goodToGo: false, fold: false, money: 30000, call: 0, selfBid: 0}
+
+let playersInThisRound = [player1, player2, player3];
+
+let gameStash = 0;
 
 function everyBodyChecked(players){
     for(let i in players){
@@ -13,7 +15,7 @@ function everyBodyChecked(players){
     return true;
 }
 
-function bid(action, player, players, bidValue){
+function bid(action, player, players, bidValue, stash){
     let relevantPlayer
     for(let i in players){
         if(players[i].userName === player.userName){
@@ -48,13 +50,19 @@ function bid(action, player, players, bidValue){
             relevantPlayer.fold = true;
             break;
     }
-    return everyBodyChecked(players);
+    
+    if(everyBodyChecked(players)){
+        for(let i in players){
+            [stash, players[i].selfBid] = [stash + players[i].selfBid, 0]
+        }
+        console.log(stash);
+    }
 }
 
 module.exports = bid;
 
-//console.log(bid("CHECK", player3, playersInThisRound, 0));
-//bid("RAISE", player1, playersInThisRound, 100);
-//bid("FOLD", player2, playersInThisRound, 0);
-//console.log(bid("CALL", player3, playersInThisRound, 0));
-//console.log(playersInThisRound);
+bid("CHECK", player3, playersInThisRound, 0, gameStash);
+bid("RAISE", player1, playersInThisRound, 100, gameStash);
+bid("CALL", player2, playersInThisRound, 0, gameStash);
+bid("CALL", player3, playersInThisRound, 0, gameStash);
+console.log(playersInThisRound);
